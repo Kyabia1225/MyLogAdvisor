6a887e365be127a420a648ad217260acd9f738bb
kyabia <kyabia@foxmail.com>
2022-04-23 01:22:54 +0800

    安心睡觉 先留个草稿

--- src/main/java/FeatureExtraction/CodeAnalysis.java
+++ src/main/java/FeatureExtraction/CodeAnalysis.java
@@ -1,4 +1,75 @@
 package FeatureExtraction;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.TryStmt;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
+import com.github.javaparser.ast.visitor.Visitable;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.SourceRoot;
+import org.checkerframework.checker.units.qual.A;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class CodeAnalysis {
+    public static void main(String[] args) {
+        String path = "E:\\MyPaper\\hadoop\\hadoop-yarn-project\\hadoop-yarn\\hadoop-yarn-client\\src\\main\\java\\org\\apache\\hadoop\\yarn\\client\\cli";
+        //ProjectRoot projectRoot = new ParserCollectionStrategy().collect(Paths.get(path));
+        SourceRoot sourceRoot = new SourceRoot(Paths.get(path));
+        CompilationUnit cu = sourceRoot.parse("", "ClusterCLI.java");
+
+        ArrayList<String> classNameList = new ArrayList<>();
+        //read all className
+        cu.accept(new VoidVisitorAdapter<List<String>>() {
+            @Override
+            public void visit(ClassOrInterfaceDeclaration cORid, List<String> classNameList) {
+                classNameList.add(cORid.getNameAsString());
+                super.visit(cORid, classNameList);
+            }
+        }, classNameList);
+        List<Feature> features = new ArrayList<>();
+        //split code block
+        for(String className:classNameList) {
+            Optional<ClassOrInterfaceDeclaration> clazz = cu.getClassByName(className);
+            if(clazz.isPresent()) {
+                List<MethodDeclaration> methods = clazz.get().getMethods();
+                for(MethodDeclaration method:methods) {
+                    String methodCodeText = method.getDeclarationAsString(false, true, true)
+                            +"\n" + method.getBody().get() + "\n";
+                    method.accept(new ModifierVisitor<Void>() {
+                        @Override
+                        public Visitable visit(TryStmt stmt, Void arg) {
+                            System.out.println(stmt.getCatchClauses());
+                            System.out.println(method.getThrownExceptions());
+                            return super.visit(stmt, arg);
+                        }
+                    }, null);
+                    Feature feature = new Feature();
+                    feature.setDirectory(cu.getStorage().get().getDirectory().toString());
+                    feature.setMethodName(method.getNameAsString());
+                    feature.setSourceCodeText(methodCodeText);
+                    features.add(feature);
+                }
+            }
+        }
+
+//        JavaParser javaParser = new JavaParser();
+//        for(Feature feature:features) {
+           // System.out.println(javaParser.parse(feature.getSourceCodeText()).getResult());
+//            methodCU.accept(new ModifierVisitor<Void>(){
+//                @Override
+//                public Visitable visit(TryStmt stmt, Void arg) {
+//                    System.out.println(stmt.getCatchClauses().toString());
+//                    return super.visit(stmt, arg);
+//                }
+//            }, null);
+        //}
+
+    }
 }
--- src/main/java/FeatureExtraction/Feature.java
+++ src/main/java/FeatureExtraction/Feature.java
@@ -14,4 +14,91 @@ public class Feature {
     private boolean logged; //label
     public Feature(){}
 
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getErrType() {
+        return errType;
+    }
+
+    public void setErrType(String errType) {
+        this.errType = errType;
+    }
+
+    public int getContainingMethodsNum() {
+        return containingMethodsNum;
+    }
+
+    public void setContainingMethodsNum(int containingMethodsNum) {
+        this.containingMethodsNum = containingMethodsNum;
+    }
+
+    public int getInvokedMethodsNum() {
+        return invokedMethodsNum;
+    }
+
+    public void setInvokedMethodsNum(int invokedMethodsNum) {
+        this.invokedMethodsNum = invokedMethodsNum;
+    }
+
+    public String getSourceCodeText() {
+        return sourceCodeText;
+    }
+
+    public void setSourceCodeText(String sourceCodeText) {
+        this.sourceCodeText = sourceCodeText;
+    }
+
+    public boolean isSettingFlag() {
+        return settingFlag;
+    }
+
+    public void setSettingFlag(boolean settingFlag) {
+        this.settingFlag = settingFlag;
+    }
+
+    public boolean isHasThrow() {
+        return hasThrow;
+    }
+
+    public void setHasThrow(boolean hasThrow) {
+        this.hasThrow = hasThrow;
+    }
+
+    public boolean isReturnSpecialValue() {
+        return returnSpecialValue;
+    }
+
+    public void setReturnSpecialValue(boolean returnSpecialValue) {
+        this.returnSpecialValue = returnSpecialValue;
+    }
+
+    public int getTryCatchBlockNum() {
+        return tryCatchBlockNum;
+    }
+
+    public void setTryCatchBlockNum(int tryCatchBlockNum) {
+        this.tryCatchBlockNum = tryCatchBlockNum;
+    }
+
+    public boolean isLogged() {
+        return logged;
+    }
+
+    public void setLogged(boolean logged) {
+        this.logged = logged;
+    }
 }
